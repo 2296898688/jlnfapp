6e0f05500fa18c50b92c6ad0cf6dfe4640c01af1
吉林农发 <admin@jlnf.com>
2026-06-12 12:07:16 +0800

    修复静态资源路径：base改为相对路径 ./

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/jlnfapp/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
